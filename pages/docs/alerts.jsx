79388092c39d542c245639620750b78dbe55d6e2
Estevan Maito <ejmaito@gmail.com>
2020-05-10 18:40:51 -0300

    fix alert with close icon padding right

--- pages/docs/alerts.jsx
+++ pages/docs/alerts.jsx
@@ -14,7 +14,7 @@ const simpleAlertCode = `
 `
 
 const alerWithCloseIconCode = `
-<div class="px-4 py-3 leading-normal text-red-700 bg-red-100 rounded-lg relative" role="alert">
+<div class="pl-4 pr-10 py-3 leading-normal text-red-700 bg-red-100 rounded-lg relative" role="alert">
   <p>A simple alert with text and a right icon</p>
   <span class="absolute right-0 inset-y-0 flex items-center mr-4">
     <svg class="fill-current h-4 w-4" role="button" viewBox="0 0 20 20"><path d="M4.293 4.293a1 1 0 011.414 0L10 8.586l4.293-4.293a1 1 0 111.414 1.414L11.414 10l4.293 4.293a1 1 0 01-1.414 1.414L10 11.414l-4.293 4.293a1 1 0 01-1.414-1.414L8.586 10 4.293 5.707a1 1 0 010-1.414z" clip-rule="evenodd" fill-rule="evenodd"></path></svg>
