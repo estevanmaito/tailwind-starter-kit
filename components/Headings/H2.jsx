aee9d24d98133b929f9c32d7f3d736729003cef8
Estevan Maito <ejmaito@gmail.com>
2020-05-10 14:09:11 -0300

    darken heading

--- components/Headings/H2.jsx
+++ components/Headings/H2.jsx
@@ -1,7 +1,7 @@
 import React from 'react'
 
 function H2({ children }) {
-  return <h2 className="text-4xl font-semibold text-gray-700">{children}</h2>
+  return <h2 className="text-4xl font-semibold text-gray-800">{children}</h2>
 }
 
 export default H2
