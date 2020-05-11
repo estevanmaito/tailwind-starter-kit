62ba08b88a7205e6a09e4409926ae366c9ef7121
Estevan Maito <ejmaito@gmail.com>
2020-05-11 17:49:22 -0300

    improve sidebar a11y

--- components/Sidebar.jsx
+++ components/Sidebar.jsx
@@ -7,21 +7,21 @@ function Sidebar({ isOpen }) {
   const router = useRouter()
 
   const navClasses = isOpen
-    ? 'w-full fixed inset-0 pt-16 h-full text-gray-600 lg:block lg:w-1/4 xl:w-1/5 bg-white z-10'
-    : 'hidden w-full text-gray-600 lg:block lg:w-1/4 xl:w-1/5 bg-white z-10'
+    ? 'w-full fixed inset-0 pt-16 h-full text-gray-800 lg:block lg:w-1/4 xl:w-1/5 bg-white z-10'
+    : 'hidden w-full text-gray-800 lg:block lg:w-1/4 xl:w-1/5 bg-white z-10'
 
   return (
     <nav className={navClasses}>
       <div className="h-full overflow-y-auto scrolling-touch lg:fixed">
         <div className="p-6 overflow-y-auto lg:p-0">
-          <h5 className="text-xs font-bold tracking-normal text-gray-500 uppercase">Components</h5>
+          <h5 className="text-xs font-bold tracking-normal text-gray-700 uppercase">Components</h5>
           <ul>
             {routes.map((r) => (
               <li
                 className={router.pathname.includes(r.url) ? 'text-gray-800 font-semibold' : ''}
                 key={r.url}>
                 <Link href={r.url}>
-                  <a className="hover:text-gray-800">{r.title}</a>
+                  <a className="inline-block hover:text-gray-900 hover:font-semibold">{r.title}</a>
                 </Link>
               </li>
             ))}
