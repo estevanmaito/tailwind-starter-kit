8dd0fa5bb20e8a3bfb7efc3511600008fe56982d
Estevan Maito <ejmaito@gmail.com>
2020-05-11 17:05:36 -0300

    increase mobile menu button sizes

--- components/Navbar.jsx
+++ components/Navbar.jsx
@@ -58,7 +58,7 @@ function Navbar({ isOpen, setIsOpen }) {
       <div className="flex items-center -mr-2 lg:hidden">
         <button className="p-2 ml-4 text-gray-600" onClick={() => setIsOpen(!isOpen)}>
           {isOpen ? (
-            <svg className="w-4 h-4 fill-current" viewBox="0 0 20 20">
+            <svg className="w-6 h-6 fill-current" viewBox="0 0 20 20">
               {/* Close icon */}
               <path
                 d="M4.293 4.293a1 1 0 011.414 0L10 8.586l4.293-4.293a1 1 0 111.414 1.414L11.414 10l4.293 4.293a1 1 0 01-1.414 1.414L10 11.414l-4.293 4.293a1 1 0 01-1.414-1.414L8.586 10 4.293 5.707a1 1 0 010-1.414z"
@@ -66,7 +66,7 @@ function Navbar({ isOpen, setIsOpen }) {
                 fillRule="evenodd"></path>
             </svg>
           ) : (
-            <svg className="w-4 h-4 fill-current" viewBox="0 0 20 20">
+            <svg className="w-6 h-6 fill-current" viewBox="0 0 20 20">
               {/* Menu icon */}
               <path
                 d="M3 5a1 1 0 011-1h12a1 1 0 110 2H4a1 1 0 01-1-1zM3 10a1 1 0 011-1h12a1 1 0 110 2H4a1 1 0 01-1-1zM3 15a1 1 0 011-1h12a1 1 0 110 2H4a1 1 0 01-1-1z"
