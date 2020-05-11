69cde499530eeb47a91e030f8f87aabf38e12ee5
Estevan Maito <ejmaito@gmail.com>
2020-05-11 19:34:17 -0300

    fix spacing and font sizes

--- components/Sidebar.jsx
+++ components/Sidebar.jsx
@@ -7,21 +7,25 @@ function Sidebar({ isOpen }) {
   const router = useRouter()
 
   const navClasses = isOpen
-    ? 'w-full fixed inset-0 pt-16 h-full text-gray-800 lg:block lg:w-1/4 xl:w-1/5 bg-white z-10'
-    : 'hidden w-full text-gray-800 lg:block lg:w-1/4 xl:w-1/5 bg-white z-10'
+    ? 'w-full fixed inset-0 pt-16 h-full text-gray-700 lg:block lg:w-1/4 xl:w-1/5 bg-white z-10'
+    : 'hidden w-full text-gray-700 lg:block lg:w-1/4 xl:w-1/5 bg-white z-10'
 
   return (
     <nav className={navClasses}>
       <div className="h-full overflow-y-auto scrolling-touch lg:fixed">
         <div className="p-6 overflow-y-auto lg:p-0">
-          <h5 className="text-xs font-bold tracking-normal text-gray-700 uppercase">Components</h5>
-          <ul>
+          <h5 className="mb-1 text-sm font-bold tracking-normal text-gray-700 uppercase lg:text-xs">
+            Components
+          </h5>
+          <ul className="text-base font-medium lg:text-sm">
             {routes.map((r) => (
               <li
-                className={router.pathname.includes(r.url) ? 'text-gray-800 font-semibold' : ''}
+                className={
+                  router.pathname.includes(r.url) ? 'text-indigo-600 font-semibold mb-1' : 'mb-1'
+                }
                 key={r.url}>
                 <Link href={r.url}>
-                  <a className="inline-block hover:text-gray-900 hover:font-semibold">{r.title}</a>
+                  <a className="inline-block py-1 hover:font-semibold">{r.title}</a>
                 </Link>
               </li>
             ))}
