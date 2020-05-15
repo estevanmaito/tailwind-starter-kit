2c305fb3b33cabfbcfccfce4e231831a120d9446
Estevan Maito <ejmaito@gmail.com>
2020-05-14 22:12:48 -0300

    add mdx

---
+++ layouts/docs.jsx
@@ -0,0 +1,22 @@
+import React, { useState } from 'react'
+import Navbar from '../components/Navbar'
+import Sidebar from '../components/Sidebar'
+
+export default (frontMatter) => {
+  return ({ children }) => {
+    const [isOpen, setIsOpen] = useState(false)
+
+    const mainClassName = isOpen
+      ? 'fixed max-w-full min-h-screen overflow-hidden lg:relative lg:w-3/4 xl:w-3/5 xl:pl-12'
+      : 'max-w-full lg:relative lg:w-3/4 xl:w-3/5 xl:pl-12'
+    return (
+      <div className="w-full px-6 mx-auto sm:max-w-screen-sm lg:max-w-screen-xl">
+        <Navbar isOpen={isOpen} setIsOpen={setIsOpen} />
+        <div className="flex mt-24">
+          <Sidebar isOpen={isOpen} />
+          <div className={mainClassName}>{children}</div>
+        </div>
+      </div>
+    )
+  }
+}
--- next.config.js
+++ next.config.js
@@ -1,7 +1,19 @@
 const isProd = process.env.NODE_ENV === 'production'
+const withMdxEnhanced = require('next-mdx-enhanced')
 
-module.exports = {
+module.exports = withMdxEnhanced({
+  layoutPath: 'layouts',
+  // defaultLayout: true,
+  fileExtensions: ['mdx'],
+  remarkPlugins: [],
+  rehypePlugins: [],
+  extendFrontMatter: {
+    process: (mdxContent, frontMatter) => {},
+    phase: 'prebuild|loader|both',
+  },
+})({
+  // pageExtensions: ['js', 'jsx', 'mdx'],
   experimental: {
     basePath: isProd ? '/tailwind-starter-kit' : '',
   },
-}
+})
