f9c2713bb3c4ce59708f0404a4ffe24718313d23
Estevan Maito <ejmaito@gmail.com>
2020-05-16 22:11:34 -0300

    add react live code block

---
+++ components/CodeBlock.jsx
@@ -0,0 +1,49 @@
+import React from 'react'
+import Highlight, { defaultProps } from 'prism-react-renderer'
+import { LiveProvider, LiveEditor, LiveError, LivePreview } from 'react-live'
+import { mdx } from '@mdx-js/react'
+
+export default ({ children, className, live, render }) => {
+  const language = className.replace(/language-/, '')
+  if (live) {
+    return (
+      <div className="overflow-hidden rounded-lg">
+        <LiveProvider
+          code={children}
+          transformCode={(code) => '/** @jsx mdx */' + `<>${code}</>`}
+          scope={{ mdx }}
+          {...defaultProps}>
+          <LivePreview className="p-4 border-t border-l border-r rounded-t-lg" />
+          <LiveEditor className="overflow-x-auto break-normal" />
+          <LiveError />
+        </LiveProvider>
+      </div>
+    )
+  }
+
+  if (render) {
+    return (
+      <div style={{ marginTop: '40px' }}>
+        <LiveProvider code={children}>
+          <LivePreview />
+        </LiveProvider>
+      </div>
+    )
+  }
+
+  return (
+    <Highlight {...defaultProps} code={children} language={language}>
+      {({ className, style, tokens, getLineProps, getTokenProps }) => (
+        <pre className={className} style={{ ...style, padding: '20px' }}>
+          {tokens.map((line, i) => (
+            <div key={i} {...getLineProps({ line, key: i })}>
+              {line.map((token, key) => (
+                <span key={key} {...getTokenProps({ token, key })} />
+              ))}
+            </div>
+          ))}
+        </pre>
+      )}
+    </Highlight>
+  )
+}
--- pages/_app.jsx
+++ pages/_app.jsx
@@ -2,12 +2,17 @@ import React from 'react'
 import App from 'next/app'
 import '../css/tailwind.css'
 import '../css/prism-dracula.css'
+import { MDXProvider } from '@mdx-js/react'
 
-class MyApp extends App {
-  render() {
-    const { Component, pageProps } = this.props
-    return <Component {...pageProps} />
-  }
+import CodeBlock from '../components/CodeBlock'
+
+const mdComponents = {
+  pre: (props) => <div {...props} />,
+  code: CodeBlock,
 }
 
-export default MyApp
+export default ({ Component, pageProps }) => (
+  <MDXProvider components={mdComponents}>
+    <Component {...pageProps} />
+  </MDXProvider>
+)
