270e821740b2d05526dfc4afddec6a5a1304e404
Estevan Maito <ejmaito@gmail.com>
2020-05-27 21:32:29 -0300

    fix theming

--- components/CodeBlock.jsx
+++ components/CodeBlock.jsx
@@ -41,18 +41,20 @@ export default ({ children, className, live, render }) => {
   }
 
   return (
-    <Highlight {...defaultProps} code={children} language={language}>
-      {({ className, style, tokens, getLineProps, getTokenProps }) => (
-        <pre className={className} style={{ ...style, padding: '20px' }}>
-          {tokens.map((line, i) => (
-            <div key={i} {...getLineProps({ line, key: i })}>
-              {line.map((token, key) => (
-                <span key={key} {...getTokenProps({ token, key })} />
-              ))}
-            </div>
-          ))}
-        </pre>
-      )}
-    </Highlight>
+    <div className="overflow-hidden rounded-lg">
+      <Highlight {...defaultProps} theme={theme} code={children.trim()} language={language}>
+        {({ className, style, tokens, getLineProps, getTokenProps }) => (
+          <pre className={className} style={{ ...style, padding: '20px' }}>
+            {tokens.map((line, i) => (
+              <div key={i} {...getLineProps({ line, key: i })}>
+                {line.map((token, key) => (
+                  <span key={key} {...getTokenProps({ token, key })} />
+                ))}
+              </div>
+            ))}
+          </pre>
+        )}
+      </Highlight>
+    </div>
   )
 }
